de45430bad1bef3fda859400b80dd782c542d30f
ELMER YUNIOR RAMIREZ MELO <yunior20ramirez@gmail.com>
2022-05-21 17:56:08 -0500

    Ingresar usuario

--- G_Proyectos/G_Proyectos/App.xaml.cs
+++ G_Proyectos/G_Proyectos/App.xaml.cs
@@ -14,7 +14,7 @@ namespace G_Proyectos
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new Views.GraficoCircular()) ;
+            MainPage = new NavigationPage(new Views.Principal()) ;
         }
         public static SQLiteConeccion SQLiteDB
         {
