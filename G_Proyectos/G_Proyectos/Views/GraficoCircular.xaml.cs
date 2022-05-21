c8b1bbbe1b5d363f8dc6ad208377c41ae558c553
Piero-21 <piero.sistemas21@gmail.com>
2022-05-21 15:09:15 -0500

    mi parte xd

---
+++ G_Proyectos/G_Proyectos/Views/GraficoCircular.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using DataChart = Microcharts.ChartEntry;
+
+namespace G_Proyectos.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GraficoCircular : ContentPage
+    {
+        public GraficoCircular()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            List<DataChart> datachartlist = new List<DataChart>
+            {
+                new DataChart(3.4f)
+                {
+                    Color = SkiaSharp.SKColor.Parse("#ff92a0"),
+                    TextColor = SkiaSharp.SKColor.Parse("#ff92a0"),
+                    Label = "Point 1",
+                    ValueLabel = "3.4"
+                },
+                new DataChart(7.3f)
+                {
+                    Color = SkiaSharp.SKColor.Parse("#0092a0"),
+                    TextColor = SkiaSharp.SKColor.Parse("#0092a0"),
+                    Label = "Point 2",
+                    ValueLabel = "7.3"
+                }
+            };
+
+            Grafico_Torta.Chart = new Microcharts.LineChart
+            {
+                
+            };
+        }
+    }
+}
