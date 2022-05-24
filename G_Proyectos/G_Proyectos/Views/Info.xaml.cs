dd53ab96b7b8436a0f8c44b37e34c8b707e38a97
ELMER YUNIOR RAMIREZ MELO <yunior20ramirez@gmail.com>
2022-05-24 15:35:31 -0500

    CAMBIO

--- G_Proyectos/G_Proyectos/Views/Info.xaml.cs
+++ G_Proyectos/G_Proyectos/Views/Info.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using G_Proyectos.SQLite;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,19 +17,23 @@ namespace G_Proyectos.Views
             InitializeComponent();
         }
 
-        private void actualizarinfo_Clicked(object sender, EventArgs e)
+        private async void actualizarinfo_Clicked(object sender, EventArgs e)
         {
+            if (true)
+            {
+                Usuario date = new Usuario
+                {
+                    NombreReal = nombre.Text,
+                    Apellido = apellido.Text,
+                    FecNacimiento = fecha_nac.Date,
+                    TelefonoP = celular.Text,
+                    TipoPersona = tipo_paciente.Text
 
-        }
+                 };
+                await App.SQLiteDB.ActualiarUsuario(date);
+                await DisplayAlert("Advetencia", "Actualizacion con exito", "OK");
 
-        private void actualizarinfo_Clicked_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void actualizarinfo_Clicked_2(object sender, EventArgs e)
-        {
-
+            }
         }
     }
 }
