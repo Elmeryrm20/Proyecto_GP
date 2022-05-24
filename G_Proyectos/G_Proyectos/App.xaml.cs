005e73e2fededd3d6ed1dcd772deac188bf81e9a
CHRISTIAN BRAYAN FERNANDEZ SALOME <20175218@sistemasunica.edu.pe>
2022-05-24 13:08:51 -0500

    inicio_actual

--- G_Proyectos/G_Proyectos/App.xaml.cs
+++ G_Proyectos/G_Proyectos/App.xaml.cs
@@ -16,7 +16,7 @@ namespace G_Proyectos
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new Views.Detalle_mes());
+            MainPage = new NavigationPage(new Views.MenuLateral());
         }
         public static SQLiteConeccion SQLiteDB
         {
